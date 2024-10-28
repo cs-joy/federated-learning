9e699af2b99028c8f9cdffc6f6f1cfc2d75bf5c9
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2024-10-28 23:57:22 +0600

    Update gleam.py

--- FL_implementation/src/datasets/gleam.py
+++ FL_implementation/src/datasets/gleam.py
@@ -65,4 +65,133 @@ def fetch_gleam(args, root, seed, test_size, seq_len):
             if idx == 0:    # demographic information
                 demo = pd.read_csv(os.path.join(path, files[0]), usecols= ['Id', 'Age', 'Gender', 'Wear glasses?', 'Annotator', 'Chair type'])
                 demo = demo.rename(columns= {'Wear glasses?': 'Glasses'})
-                demo['Annotator'] = demo['Annotator'].apply()
+                demo['Annotator'] = demo['Annotator'].apply(lambda x: int(str(x).split(' ')[-1]) - 1)
+                demo['Gender'] = demo['Gender'].replace({'M': 0, 'F': 1}).astype(int)
+                demo['Glasses'] = demo['Glasses'].replace({'No': 0, 'Yes': 1}).astype(int)
+                demo['Chair type'] = demo['Chair type'].replace({'fixed': 0, 'rolling': 1}).astype(int)
+            else:
+                # get identifier
+                identifier = path[-4:]
+
+                # read essential files
+                anno, sensor = sorted([file for file in files if not file.startswith('._') and not file.endswith('.jpg') and 'DS_Store' not in file and 'notes' not in file])
+                anno, sensor = pd.read_csv(os.path.join(path, anno), usecols= ['unix time', 'Activity'], na_values= 'w').rename(columns= {'unix time': 'Time'}), pd.read_csv(os.path.join(path, sensor)).rename(columns= {'Unix time': 'Time'})
+
+                # polish sensor name
+                sensor['Sensor'] = sensor['Sensor'].str.split().apply(lambda x: x[-1] if len(x) == 2 else x[1]).replace({'Gyroscope': 0, 'Accelerometer': 1, 'Magnetic': 2, 'Rotation': 3, 'Linear': 4, 'Gravity': 5, 'Light': 6})
+
+                # get timestamp
+                anno['Time'] = pd.to_datetime(anno['Time'], unit= 'ms')
+                sensor['Time'] = pd.to_datetime(sensor['Time'], unit= 'ms')
+
+                # convert annotation data to have 'start time', 'end time', corresponding 'activity' type
+                anno = pd.DataFrame({'start': anno['Time'].iloc[::2].values, 'end': anno['Time'].iloc[1::2].values, 'Activity': anno['Activity'].iloc[::2].values})
+
+                # fill y, z values of luminance sensors with zero
+                sensor = sensor.fillna(0)
+
+                # get indices corresponds to start & end time of each activity
+                start = sensor['Time'].searchsorted(anno['start'])
+                end = sensor['Time'].searchsorted(anno['end']) - 1
+
+                # define activity-index range dictionary
+                ref_dict = dict(zip(anno['Activity'].values.tolist(), zip(start, end)))
+
+                # assign activity
+                sensor['Activity'] = sensor.apply(lambda x: assign_acitivity(x, ref_dict), axis= 1)
+
+                # mark no activity with 'None'
+                sensor['Activity'] = sensor['Activity'].fillna('none')
+
+                # add demographical data
+                raw = pd.concat(
+                    [
+                        sensor.iloc[:, 1:-1],   # features (x, y, z sensors)
+                        demo[demo['Id'] == int(identifier)].iloc[:, 1:].reset_index(drop= True)
+                    ], axis= 1
+                ).ffill()
+
+                # add target
+                raw = pd.concat(
+                    [
+                        raw,
+                        sensor.iloc[:, -1].replace(
+                            {
+                                'none': -100,   # to be ignored by `torch.nn.CrossEntropyLoss` as it is a dominant class
+                                'eat': 0,
+                                'stair': 1,
+                                'walk': 2,
+                                'talk': 3,
+                                'drink': 4,
+                                'play_mobile': 5, 'play_game': 5, 'play game': 5, 'play_cellphone': 5,
+                                'ipad_work': 6, 'computer_work': 6, 'computer work': 6, 'computer': 6,
+                                'study': 7, 'homework': 7, 'home_work': 7
+                            }
+                        )   # target (activity)
+                    ], axis= 1
+                )
+
+                # min-max normalize based on sensor range
+                for c in range(len(MINMAX)):
+                    (xmin, xmax), (ymin, ymax), (zmin, zmax) = MINMAX[c]
+                    raw.loc[raw['Sensor'] == c, 'Value1'] = (raw.loc[raw['Sensor'] == c, 'Value1'] - xmin) / (xmax - xmin)
+                    raw.loc[raw['Sensor'] == c, 'Value2'] = (raw.loc[raw['Sensor'] == c, 'Value2'] - ymin) / (ymax - ymin)
+                    raw.loc[raw['Sensor'] == c, 'Value3'] = (raw.loc[raw['Sensor'] == c, 'Value3'] - zmin) / (zmax - zmin)
+                else:
+                    raw = raw.fillna(0)
+                
+                # scale 'Age' variable (18 ~ 21)
+                raw['Age'] -= 18
+                raw['Age'] /= (21 - 18)
+
+                # one-hot encode sensor type
+                raw = pd.concat([pd.get_dummies(raw['Sensor'], drop_first= True, prefix= 'Sensor', prefix_sep= ''), raw.iloc[:, 1:]], axis= 1)
+
+                # split into train & test set
+                train, test = raw.iloc[:-int(len(raw) * test_size)], raw.iloc[-int(len(raw) * test_size):]
+
+                # split into inputs and targets
+                train_inputs, train_targets = train.iloc[:, :-1].values, train.iloc[:, -1].values
+                test_inputs, test_targets = test.iloc[:, :-1].values, test.iloc[:, -1].values
+
+                # slice by `seq_len`
+                train_inputs_chunks, train_targets_chunks = np.split(train_inputs, np.arange(seq_len, len(train_inputs), seq_len)), np.split(train_targets, np.arange(seq_len, len(train_targets), seq_len))
+                test_inputs_chunks, test_targets_chunks = np.split(test_inputs, np.arange(seq_len, len(test_inputs), seq_len)), np.split(test_targets, np.arange(seq_len, len(test_targets), seq_len))
+
+                # pad
+                train_inputs_chunks[-1] = np.pad(train_inputs_chunks[-1], ((0, seq_len - len(train_inputs_chunks[-1])), (0, 0)), mode= 'constant', constant_values= 0)
+                train_targets_chunks[-1] = np.pad(train_targets_chunks[-1], (seq_len - len(train_targets_chunks[-1]), 0), mode= 'constant', constant_values= -100)
+
+                test_inputs_chunks[-1] = np.pad(test_inputs_chunks[-1], ((0, seq_len - len(test_inputs_chunks[-1])), (0, 0)), mode= 'constant', constant_values= 0)
+                test_targets_chunks[-1] = np.pad(test_targets_chunks[-1], (seq_len - len(test_targets_chunks[-1]), 0), mode= 'constant', constant_values= -100)
+
+                # re-assign padded inputs: i.e., (num_chunks, seq_len, in_fetures)
+                train_inputs, train_targets = np.array(train_inputs_chunks).astype(float), np.array(train_targets_chunks).astype(int)
+                test_inputs, test_targets = np.array(test_targets_chunks).astype(float), np.array(test_targets_chunks).astype(int)
+
+                # assign as a client dataset
+                clients_datasets.append(
+                    (
+                        GLEAM(f'[GLEAM] CLIENT < {identifier} > (train)', train_inputs, train_targets),
+                        GLEAM(f'[GLEAM] CLIENT < {identifier} > (test)', test_inputs, test_targets)
+                    )
+                )
+            
+            return clients_datasets
+    
+    logger.info(f'[LOAD] [GLEAM] Check if raw data exists; if not, start downloading!')
+    if not os.path.exists(os.path.join(root, 'gleam')):
+        _download(root= os.path.join(root, 'gleam'))
+        logger.info(f'[LOAD] [GLEAM] ...raw data is successfully downloaded!')
+    else:
+        logger.info(f'[LOAD] [GLEAM] ...raw data already exists!')
+    
+    logger.info(f'[LOAD] [GLEAM] Munging and splitting dataset!')
+    client_datasets = _munge_and_split(os.path.join(root, 'gleam'), seed, test_size)
+    logger.info(f'[LOAD] [GLEAM] ...munged and splitted dataset!')
+
+    args.in_features = 14
+    args.num_classes = 8
+    args.K = 38
+
+    return {}, client_datasets, args
