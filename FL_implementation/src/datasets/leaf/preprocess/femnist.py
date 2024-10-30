3890be3fa4f25debb3d6071fa6d50f53ff038dab
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2024-10-30 22:21:04 +0600

    initial commit

---
+++ FL_implementation/src/datasets/leaf/preprocess/femnist.py
@@ -0,0 +1,35 @@
+import os
+import json
+import pickle
+import logging
+import hashlib
+
+import numpy as np
+
+logger = logging.getLogger(__name__)
+
+
+
+def preprocess(root):
+    MAX_WRITERS = 100   # max numer of writers per json files
+
+    def _save_obj(obj, name):
+        pass
+
+    def _load_obj(name):
+        pass
+
+    def _parse_files(path):
+        pass
+
+    def _get_hashes(path):
+        pass
+
+    def _match_by_hashes(path):
+        pass
+
+    def _group_by_write(path):
+        pass
+
+    def _convert_to_json(path):
+        pass
