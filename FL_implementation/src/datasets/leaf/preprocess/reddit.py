89413feae05e32f7482c35d611d19b0b5c9999fb
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2024-11-01 01:29:06 +0600

    Update reddit.py

--- FL_implementation/src/datasets/leaf/preprocess/reddit.py
+++ FL_implementation/src/datasets/leaf/preprocess/reddit.py
@@ -71,9 +71,78 @@ def preprocess(root):
 
     def _convert_to_ids_and_get_length(raw, vocab):
         def _tokens_to_word_ids(tokens, vocab):
-            pass
+            return [vocab[word] for word in tokens]
 
         def _convert_to_id(container, key):
-            pass
+            transformed = []
+            for data in container[key]:
+                for sent in data:
+                    idx = _tokens_to_word_ids(sent, vocab)
+                    transformed.append(idx)
+            
+            return transformed
+        
+        for user in raw['users']:
+            raw['user_data'][user]['x'] = _convert_to_id(raw['user_data'][user], 'x')
+            raw['user_data']['user']['y'] = _convert_to_id(raw['user_data'][user], 'y')
+        
+        return raw
 
-    # 
+    # set path
+    DATASET_NAME = __file__.split('/')[-1].split('.')[0]
+    path = os.path.join(os.path.expanduser(root), DATASET_NAME)
+
+    # check if pre-processing has already done
+    _make_path(path, 'all_data')
+    _make_path(path, 'vocab')
+    _make_path(path, 'intermediate')
+    _make_path(path, 'train')
+    _make_path(path, 'test')
+
+    # adjust path since pre-processed json files are already prepared
+    if os.path.exists(os.path.join(path, 'raw', 'new_small_data')):
+        for file in os.listdir(os.path.join(path, 'raw', 'new_small_data')):
+            if 'train' in file:
+                os.replace(os.path.join(path, 'raw', 'new_small_data', file), os.path.join(path, 'intermediate', file))
+            elif 'test' in file:
+                os.replace(os.path.join(path, 'raw', 'new_small_data', file), os.path.join(path, 'intermediate', file))
+            else:
+                os.remove(os.path.join(path, 'raw', 'new_small_data', file))    # `val` data is not required...
+
+    # edit `num_samples`: don't know why but it is not correct...
+    logger.info(f'[LOAD] [LEAF - {DATASET_NAME.upper()}] Refine raw data...!')
+    train_data = _load_data(os.path.join(path, 'intermediate', 'train_data.json'))
+    test_data = _load_data(os.path.join(path, 'intermediate', 'test_data.json'))
+
+    # correct number of samples and filer tokenized samples only
+    train_data = _refine_data(train_data)
+    test_data = _refine_data(test_data)
+    logger.info(f'[LOAD] [LEAF - {DATASET_NAME.upper()}] ...finished refining raw data!')
+
+    # aggregate data
+    logger.info(f'[LOAD] [LEAF {DATASET_NAME.upper()}] Combine raw...!')
+    _save_data(os.path.join(path, 'all_data', 'train_data_refined.json'), train_data)
+    _save_data(os.path.join(path, 'all_data', 'test_data_refined.json'), test_data)
+    logger.info(f'[LOAD] [LEAF - {DATASET_NAME.upper()}] ...finished combining raw data!')
+
+    # build vocabulary
+    logger.info(f'[LOAD] [LEAF - {DATASET_NAME.upper()}] Build vocabulary...!')
+    counter = _build_counter(train_data['user_data'])
+    vocab_raw = _build_vocab(counter)
+    _save_data(os.path.join(path, 'vocab', 'reddit_vocab.json'), vocab_raw)
+
+    vocab = defaultdict(lambda: vocab_raw['unk_symbol'])
+    vocab.update(vocab_raw['vocab'])
+    logger.info(f'[LOAD] [LEAF - {DATASET_NAME.upper()}] ...vocabulary is successfully created!')
+
+    # convert tokens to index
+    logger.info(f'[LOAD] [LEAF - {DATASET_NAME.upper()}] Convert tokens into indices using vocabulary...!')
+    train_data = _convert_to_ids_and_get_length(train_data, vocab)
+    test_data = _convert_to_ids_and_get_length(test_data, vocab)
+    logger.info(f'[LOAD] [LEAF - {DATASET_NAME.upper()}] ...all token are converted into indices!')
+
+    # save processed data
+    logger.info(f'[LOAD] [LEAF - {DATASET_NAME.upper()}] Split into training and test sets...! ')
+    _save_data(os.path.join(path, 'train', 'all_data_niid_00_train.json'), train_data)
+    _save_data(os.path.join(path, 'test', 'all_data_niid_00_test.json'), test_data)
+    logger.info(f'[LOAD] [LEAF - {DATASET_NAME.upper()}] ...done splitting into training and test sets!')
