2680e4ab159152fcc6558116100313b5ee311b5b
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2024-11-03 16:51:15 +0600

    inital

---
+++ FL_implementation/coommands/original_cifar10/fedsgd_cifar10.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+# FedSGD experiments in Figure 4, 9 of (McMahan et al., 2016)
+### IID split
+python3 main.py \
+    --exp_name FedSGD_CIFAR10_CNN_IID --seed 42 --device cuda \
+    --dataset CIFAR10 \
+    --split_type iid --test_size -1 \
+    --model_name SimpleCNN --crop 24 --randhf 0.5 --randjit 0.5 --imnorm --hidden_size 64 \
+    --algorithm fedsgd --eval_fraction 1 --eval_type local --eval_every 1 --eval_metrics acc1 acc5 \
+    --K 100 --R 10000 --C 0.1 --B 0 --beta1 0 \
+    --optimizer SGD --lr 0.6 --lr_decay 0.9934 --lr_decay_step 1 --criterion CrossEntropyLoss
+
+# Pathological Non-IID split
+python3 main.py \
+    --exp_name FedSGD_CIFAR10_CN_Patho --seed 42 --device cuda \
+    --dataset CIFAR10 \
+    --split_type patho --test_size -1 \
+    --model_name SimpleCNN --crop 24 --randhf 0.5 --randjit 0.5 --imnorm --hidden_size 64 \
+    --algorithm fedsgd --eval_type local --eval_every 1 --eval_metrics acc1 acc5 \
+    --K 100 --R 10000 --C 0.1 --B 0 --beta1 0 \
+    --optimizer SGD --lr 0.45 --lr_decay 0.9934 --lr_decay_step 1 --criterion CrossEntropyLoss
---
+++ FL_implementation/coommands/original_cifar10/original_cifar10.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+
+# FedAvg esxperiments in Figure 4, 9 of (McMahan et al., 2016)
+## IID split
+python3 main.py \
+    --exp_name FedAvg_CIFAR10_CNN_IID --seed 42 --device cuda \
+    --dataset CIFAR10 \
+    --split_type iid --test_size -1 \
+    --model_name SimpleCNN --crop 24 --randhf 0.5 --randjit 0.5 --imnorm --hidden_size 64 \
+    --algorithm fedavg --eval_fraction 1 --eval_type local --eval_every 1 --eval_metrics acc1 acc5 \
+    --K 100 --R 1000 --C 1.0 --E 5 --B 50 --beta1 0 \
+    --optimizer SGD --lr 0.25 --lr_decay 0.99 --lr_decay_step 1 --criterion CrossEntropyLoss
+
+# Pathological Non-IID split
+python3 main.py \
+    --exp_name FedAvg_CIFAR10_CNN_Patho --seed 42 --device cuda \
+    --dataset CIFAR10 \
+    --split_type patho --test_size -1 \
+    --model_name SimpleCNN --crop 24 --randhf 0.5 --randjit 0.5 --imnorm --hidden_size 64 \
+    --algorithm fedavg --eval_type local --eval_every 1 --eval_metrics acc1 acc5 \
+    --K 100 --R 1000 --C 0.1 --E 5 --B 50 --beta1 0 \
+    --optimizer SGD --lr 0.5 --lr_decay 0.99 --lr_decay_step 1 --criterion CrossEntropyLoss
