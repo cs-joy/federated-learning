ca58f320490ad89f5fea49dd067e5295f1703d7a
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2024-11-03 23:36:37 +0600

    Update main.py

--- FL_implementation/main.py
+++ FL_implementation/main.py
@@ -61,4 +61,27 @@ if __name__ == "__main__":
     ####################
     # Default argument #
     ####################
-    # TODO
+    parser.add_argument('--exp_name', help= 'name of the experiment', type=str, required= True)
+    parser.add_argument('seed', help= 'global random seed', type=int, default= 5959)
+    parser.add_argument('--device', help= 'device to use; `cpu`, `cuda: GPU_NUMBER`', type= str, default= 'cpu')
+    parser.add_argument('--data_path', help= 'path to save and read raw data', type= str, default= './data')
+    parser.add_argument('--log_path', help= 'path to save logs', type= str, default= './log')
+    parser.add_argument('result_path', help= 'path to save results', type= str, default= './result')
+    parser.add_argument('use_tb', help= 'use TensorBoard for long tracking (if passed)', action= 'store_true')
+    parser.add_argument('tb_port', help= 'TensorBoard port number (valid only if `use_tb`)', type= int, default= 6006)
+    parser.add_argument('tb_host', help= 'TensorBoard host address (valid only if `use_tb`)', type=str, default= '0.0.0.0')
+
+    #####################
+    # Dataset arguments #
+    #####################
+    ## dataset configuration arguments
+    parser.add_argument('--dataset', help=''''name of dataset to use to use for an experiment (NOTE: case sensitive)
+                        - image classification datasets in `torchvision.datasets`,
+                        - text classification datasets in `torchtext.datasets`,
+                        - LEAF banchmarks [ FEMNIST | Sent140 | Shakespeare | CelebA | Reddit ],
+                        - among [ TinyImageNet | CINIC10 | SpeechCommands | BeerReviewsA | BeerReviewsL | Heart | Adult | Cover | GLEAM ]
+                        ''', type= str, required= True)
+    parser.add_argument('--test_size', help='a fraction of local hold-out dataset for evaluation (-1 for assigning pre-defined test split as local hold-out set)', type= float, choices= [Range(-1, 1.)], default= 0.2)
+    parser.add_argument('rawsmpl', help= 'a fraction of raw data to be used (valid only if one of `LEAF` datasets is used)', type= float, choices= [Range(0., 1.)], default= 1.0)
+
+    # data augmentation arguments
