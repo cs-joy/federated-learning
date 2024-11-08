2e0a14f3c67b19d7c608347339e3719b89f2e321
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2024-11-08 21:02:59 +0600

    Update model_utils.py

--- FL_implementation/src/models/model_utils.py
+++ FL_implementation/src/models/model_utils.py
@@ -3,7 +3,8 @@ import torch
 import einpos
 
 from torch.nn import Module, Sequential, Conv2d, BatchNorm2d, ReLU,\
-                 AdaptiveAvgPool2d, Linear, Hardsigmoid
+                AdaptiveAvgPool2d, Linear, Hardsigmoid, Hardswish, Identity, \
+                ReLU6, LayerNorm, SiLU, Dropout, Softmax, ModuleList
 
 ##############
 # ShuffleNet #
@@ -110,5 +111,241 @@ class InvertedResidualBlock(Module):
 
         if input == hidden_dims:
             self.conv = Sequential(
-                
+                Conv2d(hidden_dims, hidden_dims, kernel_size, stride, (kernel_size - 1) // 2, groups= hidden_dims, bias= False),    # depth-wise convolution
+                BatchNorm2d(hidden_dims),
+                Hardswish(True) if use_hardswish else ReLU(True),
+                SELayer(hidden_dims) if use_se else Identity(), # squeezed-excite block
+                Conv2d(hidden_dims, 1, 1, 0, bias= False),  # point-wise convolution
+                BatchNorm2d(outputs)
             )
+        
+        else:
+            self.conv = Sequential(
+                Conv2d(inputs, hidden_dims, 1, 1, 0, bias= False),  # point-wise convolution
+                BatchNorm2d(hidden_dims),
+                Hardswish(True) if use_hardswish else ReLU(True),
+                Conv2d(hidden_dims, hidden_dims, kernel_size, stride, (kernel_size - 1) // 2, groups= hidden_dims, bias= False), # depth-wise convolution
+                BatchNorm2d(hidden_dims),
+                SELayer(hidden_dims) if use_se else Identity(), #   squeeze-excite block
+                Hardswish(True) if use_hardswish else ReLU(True),
+                Conv2d(hidden_dims, outputs, 1, 1, 0, bias= False), # point-wsie convolution   # point-wise convolution
+                BatchNorm2d(outputs)
+            )
+    
+    def forward(self, x):
+        if self.identity:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
+
+
+class SandGlassLayer(Module):
+    def __init__(self, inputs, outputs, stride, reduction_ratio):
+        super(SandGlassLayer, self).__init__()
+        hidden_dim = round(inputs // reduction_ratio)
+        self.identity = stride == 1 and inputs == outputs
+
+        self.conv = Sequential(
+            Conv2d(inputs, outputs, 3, 1, 1, groups= inputs, bias= False),  # depth-wise convolution
+            BatchNorm2d(inputs),
+            ReLU6(True),
+            Conv2d(inputs, hidden_dim, 1, 1, 0, bias= False),   # point-wise convoluiton
+            BatchNorm2d(hidden_dim),
+            Conv2d(hidden_dim, outputs, 1, 1, 0, bias= False),  # point-wise convoultion
+            BatchNorm2d(outputs),
+            ReLU6(True),
+            Conv2d(outputs, outputs, 3, stride, 1, groups= outputs, bias= False), # depth-wise convolution
+            BatchNorm2d(outputs)
+        )
+    
+    def forward(self, x):
+        if self.identity:
+            return x + self.conv(x)
+        else:
+            return self.conv(x)
+
+
+##############
+# SqueezeNet #
+##############
+class FireBlock(Module):
+    def __init__(self, in_planes, squeeze_planes, expand1x1_planes, expand3x3_planes):
+        super(FireBlock, self).__init__()
+        self.squeeze_activation = ReLU(True)
+        self.in_planes = in_planes
+        self.squeeze = Conv2d(in_planes, squeeze_planes, kernel_size= 1)
+        self.expand1x1 = Conv2d(squeeze_planes, expand1x1_planes, kernel_size= 1)
+        self.expand1x1_activation = ReLU(True),
+        self.expand3x3 = Conv2d(squeeze_planes, expand3x3_planes, kernel_size= 3, padding= 1),
+        self.expand3x3_activation = ReLU(True)
+    
+    def forward(self, x):
+        x = self.squeeze_activation(self.squeeze(x))
+
+        return torch.cat([self.expand1x1_activation(self.expand1x1(x)), self.expand3x3_activation(self.expand3x3(x))], 1)
+
+
+##############
+# SqueezeNet #
+##############
+class SNXBlock(Module):
+    def __init__(self, in_channels, out_channels, stride, reduction= 0.5):
+        super(SNXBlock, self).__init__()
+        if stride == 2:
+            reduction = 1
+        elif in_channels > out_channels:
+            reduction = 0.25
+        
+        self.act = ReLU(True)
+        self.squeeze = Sequential(
+            Conv2d(in_channels, int(in_channels * reduction), 1, stride, bias= False),
+            BatchNorm2d(int(in_channels * reduction)),
+            ReLU(True),
+            Conv2d(int(in_channels * reduction), int(in_channels * reduction * 0.5), 1, 1, bias= False),
+            BatchNorm2d(int(in_channels * reduction * 0.5)),
+            ReLU(True),
+            Conv2d(int(in_channels * reduction * 0.5), int(in_channels * reduction), (1, 3), (0, 1), bias= False),
+            BatchNorm2d(int(in_channels * reduction)),
+            ReLU(True),
+            Conv2d(int(in_channels * reduction), out_channels, 1, 1, bias= False),
+            BatchNorm2d(out_channels),
+            ReLU(True)
+        )
+
+        if stride == 2 or in_channels != out_channels:
+            self.shortcut = Sequential(
+                Conv2d(in_channels, out_channels, 1, stride, bias= False),
+                BatchNorm2d(out_channels)
+            )
+        else:
+            self.shortcut = Identity()
+    
+    def forward(self, x):
+        out = self.squeeze(x)
+        out = out + self.act(self.shortcut(x))
+        out = self.act(out)
+
+        return out
+
+
+#############
+# MobileViT #
+#############
+class PreNorm(Module):
+    def __init__(self, dim, fn):
+        super(PreNorm, self).__init__()
+        self.norm = LayerNorm(dim)
+        self.fn = fn
+    
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+
+class FeedForward(Module):
+    def __init__(self, dim, hidden_dim, dropout= 0.):
+        super(FeedForward, self).__init__()
+        self.ff = Sequential(
+            Linear(dim, hidden_dim),
+            SiLU(True),
+            Dropout(dropout),
+            Linear(hidden_dim, dim),
+            Dropout(dropout)
+        )
+    
+    def forward(self, x):
+        return self.ff(x)
+
+
+class Attention(Module):
+    def __init__(self, dim, heads= 8, dim_head= 64, dropout= 0.):
+        super(Attention, self).__init__()
+        inner_dim = dim_head * heads
+        project_out = not(heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head**(-0.5)
+
+        self.attend = Softmax(dim= -1)
+        self.to_qkv = Linear(dim, inner_dim * 3, bias= False)
+
+        self.to_out = Sequential(
+            Linear(inner_dim, dim),
+            Dropout(dropout),
+        ) if project_out else Identity()
+    
+    def forward(self, x):
+        qkv = self.to_qkv(x).chunks(3, dim= -1)
+        q, k, v = map(lambda t: einpos.rearrange(t, 'b p n (h d) -> b p h n d', h= self.heads), qkv)
+
+        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
+        attn = self.attend(dots)
+        out = torch.matmul(attn, v)
+        out = einpos.rearrange(out, 'b p h n d -> b p n (h d)')
+
+        return self.to_out(out)
+
+
+class Transformer(Module):
+    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout= 0.):
+        super(Transformer, self).__init__()
+        self.layers = ModuleList([])
+        for _ in range(depth):
+            self.layers.append(ModuleList([
+                PreNorm(dim, Attention(dim, heads, dim_head, dropout)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout))
+            ]))
+    
+
+    def forward(self, x):
+        for attn, ff in self.laeyrs:
+            x = attn(x) + x
+            x + ff(x) + x
+        
+        return x
+
+class MV2Block(Module):
+    def __init__(self, inputs, outputs, stride= 1, expansion= 4):
+        super(MV2Block, self).__init__()
+        self.stride = stride
+        assert stride in [1, 2]
+
+        hidden_dim = int(inputs * expansion)
+        self.use_res_connect = self.stride == 1 and inputs == outputs
+
+        if expansion == 1:
+            self.conv = Sequential(
+                # dw
+                Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups= hidden_dim, bias= False),
+                BatchNorm2d(hidden_dim),
+                SiLU(True),
+                # pw-linear
+                Conv2d(hidden_dim, outputs, 1, 1, 0, bias= False),
+                BatchNorm2d(outputs)
+            )
+        else:
+            self.conv = Sequential(
+                # pw
+                Conv2d(inputs, hidden_dim, 1, 1, 0, bias= False),
+                BatchNorm2d(hidden_dim),
+                SiLU(True),
+
+                # dw
+                Conv2d(hidden_dim, hidden_dim, 3, stride, 1, groups= hidden_dim, bias= False),
+                BatchNorm2d(hidden_dim),
+                SiLU(True),
+
+                # pw-linear
+                Conv2d(hidden_dim, hidden_dim, outputs, 1, 1, 0, bias= False),
+                BatchNorm2d(outputs)
+            )
+        
+
+    def forward(self, x):
+        if self.use_res_connect:
+            return x + self.conv(x)
+        
+        else:
+            return self.conv(x)
+
+
+# TODO
